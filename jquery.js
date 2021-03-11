4fee933207560217afbe60c6f970b8ed8c65d0b7
malekmkaddem <79175484+malekmkaddem@users.noreply.github.com>
2021-03-11 01:57:51 -0800

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,51 @@
+/* Malek Mkaddem 
+
+*/
+
+$(document).ready(function() {
+                $(".buton").click(function(){
+                    $(".title").animate({
+                       
+                        fontSize : '60pt',
+                        
+                        opacity: '0.6',
+                       
+                        
+                    }, 3000,);
+                        
+                    });
+    
+                    
+                });
+$(document).ready(function() {
+                $(".buton").click(function(){
+                 $(".title").animate({
+                        fontSize : '30pt',
+                        
+                        opacity : '0.6'
+                        
+                    }, 3000,);
+                        
+                    });
+    
+                    
+                });
+$(document).ready(function() {
+                $(".buton").click(function(){
+$('.imag').css({
+    'background-color': 'purple',
+    'border' : '5px solid purple',
+    
+    
+});
+});
+});
+
+$(document).ready(function() {
+        $(".buton").click(function(){
+        $('.title').css({
+        'color' : 'purple',
+    
+});
+});
+});
